ac171886035986db989dfc20aa5ba30cbbe73146
PranitaBhatt03 <pranitabhatt00@gmail.com>
2026-04-13 06:21:24 -0700

    completed with http request methods

---
+++ Week2/Day4/put_method.py
@@ -0,0 +1,24 @@
+from fastapi import Body,FastAPI
+app=FastAPI()
+Books=[
+    {"Title":"Title1","Author":"Author1","category":"science"},
+    {"Title":"Title2","Author":"Author2","category":"science"},
+    {"Title":"Title3","Author":"Author3","category":"history"},
+    {"Title":"Title4","Author":"Author4","category":"english"},
+    {"Title":"Title5","Author":"Author2","category":"history"},
+]
+#GET
+@app.get("/books")
+async def read_books():
+    return Books
+#POST
+@app.post("/books/create_book")   #creating a post request to add a new book to the list of books
+async def create_book(new_book=Body()):     #new book is the parameter which we are getting from the body of the request
+    Books.append(new_book) 
+#PUT
+@app.put("/books/update_book")
+async def update_book(updated_book=Body()):
+    for i in range(len(Books)):
+        if Books[i].get('Title').casefold()==updated_book.get('Title').casefold():  #via indexing it updates the value of the book
+            Books[i]=updated_book
+            
