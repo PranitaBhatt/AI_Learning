fd3c49aeb26f3a355819126db7fb13b8f2676252
PranitaBhatt03 <pranitabhatt00@gmail.com>
2026-04-28 12:01:01 -0700

    Completed with  alemibc data

---
+++ Week3/Day8/TodoApp/main.py
@@ -0,0 +1,13 @@
+from fastapi import FastAPI, Depends, HTTPException, status, Path
+import models
+from database import engine
+from routers import auth,todos,admin,users #this refers to the routers packages which we build for authorization point auth.py file
+
+
+
+app = FastAPI()
+models.Base.metadata.create_all(bind=engine)
+app.include_router(auth.router) #this will help fecthing router from auth file
+app.include_router(todos.router) #will fecth todos.py file
+app.include_router(admin.router)
+app.include_router(users.router)
